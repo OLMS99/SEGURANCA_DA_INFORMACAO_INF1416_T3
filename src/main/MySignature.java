ce6a04fd2c3f425068173b567413491fa3edf670
OLMS99 <49499665+OLMS99@users.noreply.github.com>
2024-04-21 15:12:04 +0000

    Commit Inicial do projeto, criação dos arquivos Mysignature.java e MySignatureTest.java

---
+++ src/main/MySignature.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+import java.security.*;
+import javax.crypto.*;
+
+public class MySignature {
+	public static void main(String[] args){}
+
+	//Padrões de assinatura suportados:
+	//HashSet<String> padroesSuportadosAss = new HashSet<String>(Arrays.asList("MD5withRSA", "SHA1withRSA", "SHA256withRSA", "SHA512withRSA", "SHA256withECDSA"));
+	//campos para guardar [CriptografiaD]with[CriptografiaA]?
+	//getInstance(String padraoAssinatura)
+	//initSign
+	//update
+	//sign
+	//initVerify
+	//verify
+	private static String HexCodeString(byte[] hexCode) {
+		StringBuffer buf = new StringBuffer();
+
+		for (int i = 0; i < hexCode.length; i++) {
+			String hex = Integer.toHexString(0x0100 + (hexCode[i] & 0x00FF)).substring(1);
+			buf.append((hex.length() < 2 ? "0" : "") + hex);
+		}
+		return buf.toString();
+	}
+}
