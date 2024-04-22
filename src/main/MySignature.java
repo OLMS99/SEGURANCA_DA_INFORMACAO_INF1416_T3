ade9021cfe7cf3067b7d8c93cba7f559d6690f40
OLMS99 <49499665+OLMS99@users.noreply.github.com>
2024-04-22 21:39:49 +0000

    Implementação do padrão singleton e analise de possível implementação

--- src/main/MySignature.java
+++ src/main/MySignature.java
@@ -9,17 +9,61 @@ import java.security.*;
 import javax.crypto.*;
 
 public class MySignature {
-	public static void main(String[] args){}
+	private MessageDigest digestTipo;
+	private KeyPairGenerator keyGen;
+	
+	public static void main(String[] args){
+		if (args.length !=2) {
+			System.err.println("Usage: java DigitalSignatureExample signatureStandard text");
+			System.exit(1);
+		}
+		String signatureStandard = args[0];
+		String plainText = args[1];
 
-	//Padrões de assinatura suportados:
-	//HashSet<String> padroesSuportadosAss = new HashSet<String>(Arrays.asList("MD5withRSA", "SHA1withRSA", "SHA256withRSA", "SHA512withRSA", "SHA256withECDSA"));
-	//campos para guardar [CriptografiaD]with[CriptografiaA]?
-	//getInstance(String padraoAssinatura)
-	//initSign
-	//update
-	//sign
-	//initVerify
-	//verify
+		MySignature signningProcess = MySignature.getInstance(signatureStandard);
+		
+	}
+	
+	private static class SingletonHelper {
+		try{
+			private static final MySignature MD5withRSA = new MySignature("MD5","RSA");
+			private static final MySignature SHA1withRSA = new MySignature("SHA1","RSA");
+			private static final MySignature SHA256ithRSA = new MySignature("SHA256","RSA");
+			private static final MySignature SHA512withRSA = new MySignature("SHA512","RSA");
+			private static final MySignature SHA512withECDSA = new MySignature("SHA512","ECDSA");
+			
+		} catch (Exception e) {
+			System.err.println("Erro ao iniciar a classe MySignature");
+			System.exit(1);
+		}
+	}
+	private MySignature(String tipoDigest, String keyGen){
+		if (tipoDigest.equals("SHA1") || tipoDigest.equals("SHA256") || tipoDigest.equals("SHA512")) {
+			this.digestTipo = MessageDigest.getInstance(tipoDigest.substring(0, 3) + "-" + tipoDigest.substring(3));
+		} 
+		else {
+			this.digestTipo = MessageDigest.getInstance(tipoDigest);
+		}
+		this.keyGen = KeyPairGenerator.getInstance(keyGen);
+	}
+	
+	public static MySignature getInstance(String padraoAssinatura){
+		//Padrões de assinatura suportados:
+		HashSet<String> padroesSuportadosAss = new HashSet<String>(Arrays.asList("MD5withRSA", "SHA1withRSA", "SHA256withRSA", "SHA512withRSA", "SHA256withECDSA"));
+		if(!padroesSuportadosAss.contains(padraoAssinatura)){
+			System.err.println("Padrão de assinatura não suportado");
+			System.exit(1);
+		}
+		return instance;
+	}
+	//genKeys
+	//makeDigest
+	//initSign(keypair.getPrivate())
+	//update(text)
+	//update(text, offset, length)
+	//sign() returns signature
+	//initVerify(keypair.getPublic())
+	//verify(signature)
 	private static String HexCodeString(byte[] hexCode) {
 		StringBuffer buf = new StringBuffer();
 
