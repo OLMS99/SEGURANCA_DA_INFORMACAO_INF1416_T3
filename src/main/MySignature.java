9f111c620762213257b001abfff840c3a56d4e9c
Ajnus <ajnasoares@hotmail.com>
2024-04-22 20:31:18 -0300

    identa códigos

--- src/main/MySignature.java
+++ src/main/MySignature.java
@@ -8,69 +8,88 @@ import java.util.*;
 import java.security.*;
 import javax.crypto.*;
 
-public class MySignature {
+public class MySignature
+{
 	private MessageDigest digestTipo;
 	private KeyPairGenerator keyGen;
 	
-	public static void main(String[] args){
-		if (args.length !=2) {
-			System.err.println("Usage: java DigitalSignatureExample signatureStandard text");
-			System.exit(1);
-		}
+	public static void main(String[] args)
+	{
 		String signatureStandard = args[0];
 		String plainText = args[1];
 
+		if (args.length !=2)
+		{
+			System.err.println("Usage: java DigitalSignatureExample signatureStandard text");
+			System.exit(1);
+		}
+
 		MySignature signningProcess = MySignature.getInstance(signatureStandard);
-		
 	}
 	
-	private static class SingletonHelper {
-		try{
+	private static class SingletonHelper
+	{
+		try
+		{
 			private static final MySignature MD5withRSA = new MySignature("MD5","RSA");
 			private static final MySignature SHA1withRSA = new MySignature("SHA1","RSA");
 			private static final MySignature SHA256ithRSA = new MySignature("SHA256","RSA");
 			private static final MySignature SHA512withRSA = new MySignature("SHA512","RSA");
 			private static final MySignature SHA512withECDSA = new MySignature("SHA512","ECDSA");
 			
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			System.err.println("Erro ao iniciar a classe MySignature");
 			System.exit(1);
 		}
 	}
-	private MySignature(String tipoDigest, String keyGen){
-		if (tipoDigest.equals("SHA1") || tipoDigest.equals("SHA256") || tipoDigest.equals("SHA512")) {
+
+	private MySignature(String tipoDigest, String keyGen)
+	{
+		if (tipoDigest.equals("SHA1") || tipoDigest.equals("SHA256") || tipoDigest.equals("SHA512"))
+		{
 			this.digestTipo = MessageDigest.getInstance(tipoDigest.substring(0, 3) + "-" + tipoDigest.substring(3));
 		} 
-		else {
+		else
+		{
 			this.digestTipo = MessageDigest.getInstance(tipoDigest);
 		}
+
 		this.keyGen = KeyPairGenerator.getInstance(keyGen);
 	}
 	
-	public static MySignature getInstance(String padraoAssinatura){
-		//Padrões de assinatura suportados:
+	public static MySignature getInstance(String padraoAssinatura)
+	{
+		// Padrões de assinatura suportados:
 		HashSet<String> padroesSuportadosAss = new HashSet<String>(Arrays.asList("MD5withRSA", "SHA1withRSA", "SHA256withRSA", "SHA512withRSA", "SHA256withECDSA"));
+
 		if(!padroesSuportadosAss.contains(padraoAssinatura)){
 			System.err.println("Padrão de assinatura não suportado");
 			System.exit(1);
 		}
 		return instance;
 	}
-	//genKeys
-	//makeDigest
-	//initSign(keypair.getPrivate())
-	//update(text)
-	//update(text, offset, length)
-	//sign() returns signature
-	//initVerify(keypair.getPublic())
-	//verify(signature)
-	private static String HexCodeString(byte[] hexCode) {
+
+	// genKeys
+	// makeDigest
+	// initSign(keypair.getPrivate())
+	// update(text)
+	// update(text, offset, length)
+	// sign() returns signature
+	// initVerify(keypair.getPublic())
+	// verify(signature)
+
+	private static String HexCodeString(byte[] hexCode)
+	{
 		StringBuffer buf = new StringBuffer();
 
-		for (int i = 0; i < hexCode.length; i++) {
+		for (int i = 0; i < hexCode.length; i++)
+		{
 			String hex = Integer.toHexString(0x0100 + (hexCode[i] & 0x00FF)).substring(1);
 			buf.append((hex.length() < 2 ? "0" : "") + hex);
 		}
+
 		return buf.toString();
 	}
 }
--- src/test/MySignatureTest.java
+++ src/test/MySignatureTest.java
@@ -3,15 +3,19 @@
 // Olavo Lucas     - 1811181
 // -------------------------
 
-public class MySignatureTest {
-	public static void main(String[] args){}
-	//receber o padrão de assinatura e a string que deve ser assinada, nesta ordem, como argumento na linha de comando
+public class MySignatureTest
+{
+	public static void main(String[] args)
+	{
+		
+	}
+	// receber o padrão de assinatura e a string que deve ser assinada, nesta ordem, como argumento na linha de comando
 	
-	//gerar o par de chaves assimétricas para gerar a assinatura digital da string recebida na linha de comando
+	// gerar o par de chaves assimétricas para gerar a assinatura digital da string recebida na linha de comando
 	
-	//instanciar e usar os metodos da classe MySignature para gerar e verificar a assinatura digital da string recebida na linha de comando
+	// instanciar e usar os metodos da classe MySignature para gerar e verificar a assinatura digital da string recebida na linha de comando
 
-	//imprimir, na saída padrão, todos os passos executados para gerar a assinatura digital da string no padrão solicitado
+	// imprimir, na saída padrão, todos os passos executados para gerar a assinatura digital da string no padrão solicitado
 	
-	//imprimir, na saída padrão, o resumo de mensagem (digest) e a assinatura digital no formato hexadecimal
+	// imprimir, na saída padrão, o resumo de mensagem (digest) e a assinatura digital no formato hexadecimal
 }
